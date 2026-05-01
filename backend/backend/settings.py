0a7fa807db0745d8330ab8b0a171e750955bb56e
MAFAZ9416 <mafaz9416@gmail.com>
2026-05-01 18:19:25 +0530

    E-commerce_Website

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,15 +81,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('db_name'),
-        'USER' : config('db_user'),
-        'PASSWORD' : config('db_password'),
-        'HOST' : config('db_host'),
-        'PORT' : config('db_port'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
